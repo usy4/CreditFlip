b7986f2c8021da2eacc5d34d6db1c0244cfa354b
usy4 <68358524+usy4@users.noreply.github.com>
2022-11-19 10:59:22 +0300

    Update Main.php

--- src/usy4/CreditFlip/Main.php
+++ src/usy4/CreditFlip/Main.php
@@ -3,17 +3,34 @@
 namespace usy4\CreaditFlip;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\player\Player;
 
 use usy4/CreditFlip/Commands/CreditFlipCommand;
 
 class Main extends PluginBase {
     
-    public function onEnable() : void{
-        $this->getServer()->getCommandMap()->register($this->getName(), new SuggestionsCommand($this)); 
+    public $credits;
+  
+    public function onEnable(): void{
+
+       $this->credits = $this->getServer()->getPluginManager()->getPlugin("Credits");
+       $this->getServer()->getCommandMap()->register($this->getName(), new SuggestionsCommand($this)); 
     }
    
     public function TrailsOrHeads{
         return mt_rand(0,1);
     } // 0=trails, 1=heads.
+
+    public function getCredits(Player $player){
+        return $this->credits->reducedCredits($player);
+    }
+
+     public function addCredits(Player $player, int $amount){
+        return $this->credits->reducedCredits($player, $amount);
+    }
+
+     public function reduceCredits(Player $player, $amount){
+        return $this->credits->reduceCredits($player, $amount);
+    }
  
 }
