b96fbc2504adea48392346a372af5a8b12f066e3
usy4 <68358524+usy4@users.noreply.github.com>
2022-11-19 09:52:59 +0300

    Create Main.php

---
+++ src/CreditFlip/Main.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace usy4\CreaditFlip;
+
+use pocketmine\plugin\PluginBase;
+
+use usy4/CreditFlip/CreditFlipCommand;
+
+class Main extends PluginBase {
+    
+    public function onEnable() : void{
+        $this->getServer()->getCommandMap()->register($this->getName(), new SuggestionsCommand($this)); 
+    }
+   
+    public function TrailsOrHeads{
+        return mt_rand(0,1);
+    } // 0=trails, 1=heads.
+ 
+
+}
